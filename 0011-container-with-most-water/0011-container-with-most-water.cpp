ec48843e1de7c93741ff4c957ea944e537740aee
Sunil Prasad Yadav <122697413+SunilCs7@users.noreply.github.com>
2024-08-05 09:53:10 +0530

    Time: 66 ms (31.90%), Space: 61.7 MB (13.38%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int i = 0;
-        int j = height.size() - 1;
-        int maxWater = 0;
+        int i = 0;               // Left pointer
+        int j = height.size() - 1; // Right pointer
+        int maxWater = 0;        // Variable to store the maximum water
 
         while (i < j) {
-            int w = j - i;
-            int h = min(height[i], height[j]);
-            int area = w * h;
-            maxWater = max(maxWater, area);
+            int w = j - i;                  // Width between the two lines
+            int h = min(height[i], height[j]); // Height of the container
+            int area = w * h;               // Calculate the area
+            maxWater = max(maxWater, area); // Update the maximum water
+
+            // Move the pointer pointing to the shorter line
             if (height[i] < height[j]) {
                 i++;
             } else {
@@ -17,6 +19,6 @@ public:
             }
         }
 
-        return maxWater;
+        return maxWater; // Return the maximum water found
     }
 };
